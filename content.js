5667ffa42238a3d98c6b21dfddf92f0abf4a87f5
shergurj <jjtrolled@gmail.com>
2022-05-24 09:43:17 -0400

    change word detection to use regex like the original extension did

--- content.js
+++ content.js
@@ -1,57 +1,49 @@
 // making half of the letters in a word bold
 function highlightText(sentenceText) {
   return sentenceText
-    .split(' ')
-    .map((word) => {
-      // special case - hyphenated compound word, e.g. video-game
-      if (word.includes('-')) {
-        return word.split('-').map((component) => highlightText(component)).join('-')
-      }
-      const hasNumber = /\d/
-      if (hasNumber.test(word)) {
-        return word
-      }
-      const length = word.length
-      let midPoint = 1
-      if (length > 3) midPoint = Math.round(length / 2)
-      const firstHalf = word.slice(0, midPoint)
-      const secondHalf = word.slice(midPoint)
-      const htmlWord = `<br-bold class="br-bold">${firstHalf}</br-bold>${secondHalf}`
-      return htmlWord
-    })
-    .join(' ')
+    .replace(/\p{L}+/gu, (word) => {
+      const { length } = word;
+      let midPoint = 1;
+      if (length > 3) midPoint = Math.round(length / 2);
+      const firstHalf = word.slice(0, midPoint);
+      const secondHalf = word.slice(midPoint);
+      const htmlWord = `<br-bold>${firstHalf}</br-bold>${secondHalf}`;
+      return htmlWord;
+    });
 }
 
 function main() {
   // check if we have already highlighted the text
-  const boldedElements = document.getElementsByTagName('br-bold')
-  if (boldedElements.length > 0) {
-    for (const element of boldedElements) {
-      element.classList.toggle('br-bold')
-    }
-    return
+  const boldedElements = document.getElementsByTagName('br-bold');
+
+  // only add br bold to body element
+  document.body.classList.toggle('br-bold');
+
+  if (boldedElements.length) {
+    // end if no br-bold elements found on the page
+    return;
   }
 
   // setting global styles
-  var style = document.createElement('style')
-  style.textContent = '.br-bold { font-weight: bold !important; display: inline; }'
-  document.head.appendChild(style)
+  const style = document.createElement('style');
+  style.textContent = '.br-bold br-bold { font-weight: bold !important; display: inline; line-height: var(--br-line-height,initial); }';
+  document.head.appendChild(style);
 
-  let tags = ['p', 'font', 'span', 'li', 'a']
+  const tags = ['p', 'font', 'span', 'li', 'a'];
 
-  const parser = new DOMParser()
+  const parser = new DOMParser();
   tags.forEach((tag) => {
-    for (let element of document.getElementsByTagName(tag)) {
-      const n = parser.parseFromString(element.innerHTML, 'text/html')
+    for (const element of document.getElementsByTagName(tag)) {
+      const n = parser.parseFromString(element.innerHTML, 'text/html');
       const textArrTransformed = Array.from(n.body.childNodes).map((node) => {
         if (node.nodeType === Node.TEXT_NODE) {
-          return highlightText(node.nodeValue)
+          return highlightText(node.nodeValue);
         }
-        return node.outerHTML
-      })
-      element.innerHTML = textArrTransformed.join(' ')
+        return node.outerHTML;
+      });
+      element.innerHTML = textArrTransformed.join(' ');
     }
-  })
+  });
 }
 
-main()
+main();
